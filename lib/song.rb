d8a0ce07a14d4d241726d50bd8ec6768b6b36220
Amanda Ackerman <ackerm44@gmail.com>
2017-10-30 20:14:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,8 +50,8 @@ class Song
     name.delete! ".mp3"
     name.delete! "-"
     array = name.split(/\s\s/)
-    @artist_name = array[0]
-    @name = array[1]
+    self.artist_name = array[0]
+    self.name = array[1]
 
 
   end
