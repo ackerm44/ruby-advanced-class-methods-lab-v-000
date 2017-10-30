17f83cdaac5eff6e84a86cf5b1d465b90bd93b05
Amanda Ackerman <ackerm44@gmail.com>
2017-10-30 19:51:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,8 +34,8 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-      if find_by_name(name)
-        song.name
+      if self.find_by_name(name) != nil
+        self.find_by_name(name)
       else
         create_by_name(name)
       end
